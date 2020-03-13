0ef66924e30a1188d776bfafd848df94481f557e
taron-jamal <61537513+taron-jamal@users.noreply.github.com>
2020-03-13 02:32:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Prompt the user for first number.
+// Stores that first number.
+var input1 = prompt("Enter first number:");
+
+// Prompt the user for the second number.
+// Stores that second number.
+var input2 = prompt("Enter second number:")
+
+// Responds with the sum of two numbers by using an alert.
+var sumCalc = Number(input1) + Number(input2);
+alert(sumCalc);
